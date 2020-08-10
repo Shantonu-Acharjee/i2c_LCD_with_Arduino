3cf724a7b7d7c5e3795d787407442150153a3147
shantonuacharjee <shantonuacharjee@gmail.com>
2020-08-11 01:19:11 +0600

    i2c LcdArduino

---
+++ i2c_LCD_with_Arduino.ino
@@ -0,0 +1,19 @@
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x3F,16,2);  
+void setup()
+{
+  lcd.init();// initialize the lcd 
+  // Print a message to the LCD.
+  lcd.backlight();
+  lcd.setCursor(0,0);
+  lcd.print("  Hello, world!");
+  lcd.setCursor(0,1);
+  lcd.print("ShantonuAcharjee");
+}
+
+
+void loop()
+{
+}
